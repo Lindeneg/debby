ad287f5d70a063409d961e19cc312464a5d2d1dd
lindeneg <christian@lindeneg.org>
2024-06-07 00:01:07 +0200

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <glm/glm.hpp>
 
 int main(int argc, char *argv[]) {
     std::cout << "Hello world!\n";
