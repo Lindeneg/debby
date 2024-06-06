14ce382eeccd22f87e4d4b9cdd5b5e5c974de91e
lindeneg <christian@lindeneg.org>
2024-06-07 00:12:06 +0200

    lua

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,13 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <glm/glm.hpp>
+#include <imgui/imgui.h>
+#include <sol/sol.hpp>
 
 int main(int argc, char *argv[]) {
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
+    SDL_Init(SDL_INIT_EVERYTHING);
     std::cout << "Hello world!\n";
     return 0;
 }
