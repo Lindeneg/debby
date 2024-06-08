24b922b29c94fb11e59374bd1dc1db0e70e12955
lindeneg <christian@lindeneg.org>
2024-06-09 01:29:33 +0200

    test drawing

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,19 @@
 
 #include "./game.h"
 #include "./log.h"
+#include "globals.h"
+#include "screen.h"
 
 int main(int argc, char *argv[]) {
     debby::log::set_level(debby::log::LOG_LEVEL_VERBOSE);
-    if (!debby::game::initialize(800, 600)) {
+    if (!debby::game::initialize()) {
         return EXIT_FAILURE;
     }
-    // debby::game::run();
+    debby::screen::draw_line(glm::vec2(0, 0), glm::vec2(100, 100),
+                             debby::color::red);
+    debby::screen::swap_buffers();
+    debby::screen::render_present();
+    debby::game::run();
     debby::game::destroy();
     return EXIT_SUCCESS;
 }
